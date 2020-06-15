5da75dc6bc03f0cdba02aa6e383f56040f124610
Adam Arling <adam.arling@northwestern.edu>
2020-06-15 16:44:57 -0500

    Add new Google slide graphic

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,21 +13,30 @@ const IndexPage = () => (
     {/* <section className="section">
       <h1 className="title is-primary">Furbaby</h1>
     </section> */}
-    <Image />
-    <section className="section">
-      <div className="container has-text-centered">
-        <h2
-          className="subtitle has-text-white is-uppercase"
-          style={{ letterSpacing: "1rem" }}
-        >
-          Coming soon...
-        </h2>
+
+    <figure className="image is-16by9">
+      <iframe
+        className="has-ratio"
+        src="https://docs.google.com/presentation/d/e/2PACX-1vQKsHuTtpD9LmeC1GgXfRLwH6gAXPN5dzu1rRmGZboEK7dIetJdYBvB8WeUITFHf2rl6ZlwqbkVrTOY/embed?start=true&loop=true&delayms=3000"
+        frameBorder="0"
+        width="1440"
+        height="839"
+        allowFullScreen={true}
+      ></iframe>
+    </figure>
+
+    <section
+      className="section has-text-white has-text-centered"
+      style={{ fontFamily: "monospace" }}
+    >
+      <div>
         <figure>
           <audio controls src={mp3}>
             Your browser does not support the
             <code>audio</code> element.
           </audio>
         </figure>
+        <p>New trashy garage single "Boxed In" coming soon...fur u.</p>
       </div>
     </section>
   </Layout>
